c79b9b62a22bd54475263cbcd35662706ddf0ccd
devyanshuk <devkoirala10@gmail.com>
2023-08-06 23:51:11 +0200

    added logging to gameEnvironment class

--- Quoridor.Core/Quoridor.Core/Game/GameEnvironment.cs
+++ Quoridor.Core/Quoridor.Core/Game/GameEnvironment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Quoridor.Core.Utils;
 using Quoridor.Core.Entities;
+using Quoridor.Common.Logging;
 using Quoridor.Core.Extensions;
 using Quoridor.Core.Environment;
 using Quoridor.Core.Utils.CustomExceptions;
@@ -16,6 +17,8 @@ namespace Quoridor.Core.Game
         public int Turn { get; private set; }
         public List<IPlayer> Players { get; private set; }
         public HashSet<IWall> Walls { get; private set; } = new HashSet<IWall>();
+
+        private readonly ILogger _log = Logger.InstanceFor<GameEnvironment>();
 
         public GameEnvironment(
             IBoard board)
@@ -36,6 +39,8 @@ namespace Quoridor.Core.Game
                 Players = new List<IPlayer>();
             }
             Players.Add(player);
+            _log.Info($@"Adding player '{player.Id}'. Available players: '{
+                string.Join(", ", Players.Select(p => p.Id))}'");
         }
 
         public void ChangeTurn()
@@ -54,27 +59,42 @@ namespace Quoridor.Core.Game
         public void MovePlayer(Direction dir)
         {
             var player = Players[Turn];
+
+            _log.Info($"Moving player '{player.Id}' '{dir}'...");
+
             var newPos = TryMove(player.CurrentPos, dir);
             player.CurrentPos = newPos;
+
+            _log.Info($"Moved player '{player.Id}' to '{newPos}'");
         }
 
 
         public void AddWall(Vector2 from, Direction placement)
         {
+            _log.Info($"Attempting to add '{placement}ern' wall from '{from}'");
+
             if (!_board.WithinBounds(from))
-                throw new InvalidWallException($"{placement}ern wall from '{from}' could not be added. Invalid dimension.");
+            {
+                var errorMessage = $"{placement}ern wall from '{from}' could not be added. Invalid dimension.";
+                _log.Error(errorMessage);
+                throw new InvalidWallException(errorMessage);
+            }
 
             var walls = GetWallsForAffectedCells(from, placement);
             if (walls.All(w => _board.GetCell(w.From).IsAccessible(w.Placement)))
             {
+                _log.Info($"Wall validity check complete. Adding wall '{from}': '{placement}' to the board");
                 foreach(var wall in walls)
                 {
                     _board.GetCell(wall.From).AddWall(wall);
+                    _log.Info($"Cell {wall.From} is now inaccessible on the '{wall.Placement}ern' side");
                 }
             }
             else throw new WallAlreadyPresentException($"{placement}ern wall from '{from}' already present");
 
             Walls.Add(walls.First());
+
+            _log.Info($"Successfully added '{placement}ern' wall from '{from}'");
 
             Players?[Turn]?.DecreaseWallCount();
         }
@@ -102,7 +122,7 @@ namespace Quoridor.Core.Game
             var neighbors = _board.Neighbors(_board.GetCell(currPos));
             return neighbors.All(n => !n.Position.Equals(newPos));
         }
-        
+
         public IEnumerable<IWall> GetWallsForAffectedCells(Vector2 from, Direction placement)
         {
             var wall = CreateAndValidateWall(from, placement);
@@ -119,7 +139,10 @@ namespace Quoridor.Core.Game
             var dir2 = wall2.From.GetDirFor(wall4.From);
 
             if (!_board.GetCell(wall.From).IsAccessible(dir1) && !_board.GetCell(wall2.From).IsAccessible(dir2))
-                throw new WallIntersectsException($"wall '{from}' : '{placement}' intersects with a previously added wall");
+            {
+                var errorMessage = $"wall '{from}' : '{placement}' intersects with a previously added wall";
+                throw new WallIntersectsException(errorMessage);
+            }
 
             return new List<IWall> { wall, wall2, wall3, wall4 };
         }
@@ -135,7 +158,9 @@ namespace Quoridor.Core.Game
                 || (wall.From.X >= _board.Dimension - 1 && wall.IsHorizontal())
                 || (wall.From.Y >= _board.Dimension - 1 && wall.IsVertical()))
             {
-                throw new InvalidWallException($"{dir}ern wall from '{from} not possible.'");
+                var errorMessage = $"{dir}ern wall from '{from} not possible.'";
+                _log.Error(errorMessage);
+                throw new InvalidWallException(errorMessage);
             }
             return wall;
         }
@@ -145,15 +170,32 @@ namespace Quoridor.Core.Game
             var newPos = currentPos.GetPosFor(dir);
             var player = Players[Turn];
 
+            _log.Info($@"Trying to check if it's possible to move player '{player.Id}' currently at '{
+                player.CurrentPos}' '{dir}' to '{newPos}' from '{currentPos}'");
+
             if (!_board.WithinBounds(newPos))
-                throw new InvalidAgentMoveException($"player '{player.Id}' cannot move to '{newPos}'. Invalid move position");
+            {
+                var errorMessage = $"player '{player.Id}' cannot move to '{newPos}'. Invalid move position";
+                _log.Error(errorMessage);
+                throw new InvalidAgentMoveException(errorMessage);
+            }
 
             if (NewMoveBlockedByWall(currentPos, newPos))
-                throw new NewMoveBlockedByWallException($"player '{player.Id}' cannot move to '{newPos}' since it's blocked by a wall");
+            {
+                var errorMessage = $"player '{player.Id}' cannot move to '{newPos}' since it's blocked by a wall";
+                _log.Error(errorMessage);
+                throw new NewMoveBlockedByWallException(errorMessage);
+            }
 
-            var anotherPlayerExistsInNewPos = Players.FirstOrDefault(p => p.CurrentPos.Equals(newPos)) != null;
+            var playerInNewPos = Players.FirstOrDefault(p => p.CurrentPos.Equals(newPos));
 
-            return anotherPlayerExistsInNewPos ? TryMove(newPos, dir) : newPos;
+            if (playerInNewPos != null)
+            {
+                _log.Info($"Player '{playerInNewPos.Id}' already is in '{newPos}'. Trying to jump '{dir}' from '{newPos}'");
+                return TryMove(newPos, dir);
+            }
+            _log.Info($"No player found in '{newPos}'. Moving player '{player.Id}' from '{player.CurrentPos}' to '{newPos}'");
+            return newPos;
         }
     }
 }
